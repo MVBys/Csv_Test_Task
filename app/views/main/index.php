1c0056eb2663fe154349a7e58418b61b104fe752
MVbys <bismaxmail@gmail.com>
2022-08-16 21:24:45 +0300

    main index view add comtent

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -1,1 +1,15 @@
-<h1>Index</h1>
+<h1 class="display-5 fw-normal">Import data</h1>
+
+
+<form>
+    <div class="col-12 mb-4">
+        <input class="form-control form-control-lg" id="formFileLg" type="file">
+    </div>
+
+
+    <button class="w-100 btn btn-primary btn-lg" type="submit">Import</button>
+
+</form>
+
+<hr class="my-4">
+<button class="w-100 btn btn-danger btn-lg" type="submit">Clear all records</button>
