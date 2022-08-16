fe171e3eb05e51595d2cb46f73040b6ca7a1808c
MVbys <bismaxmail@gmail.com>
2022-08-16 23:49:26 +0300

    add modle Record

---
+++ app/models/Record.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class Record extends Model
+{
+    public function getRecords()
+    {
+        $result = $this->db->query('SELECT uid, name, age, email, phone, gender description FROM records');
+        return $result;
+    }
+}
