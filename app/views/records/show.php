533053aa492645d04db256759cabc1603e9d56d8
MVbys <bismaxmail@gmail.com>
2022-08-16 21:27:59 +0300

    add view records show

--- app/views/records/show.php
+++ app/views/records/show.php
@@ -1,1 +1,19 @@
-<h1>show</h1>
+<h1 class="display-5 fw-normal">Records</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+    </tbody>
+</table>
