66aae921bd81dd2c3b306854387b6fec2e6672c7
MVbys <bismaxmail@gmail.com>
2022-08-16 16:34:08 +0300

    router v1

---
+++ app/controllers/MainController.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\core\controllers;
+
+class MainController
+{
+
+    public function index()
+    {
+        echo 'controller index';
+    }
+}
--- app/core/Router.php
+++ app/core/Router.php
@@ -9,8 +9,55 @@ class Router
 
     public function __construct()
     {
-        echo ' router';
+        $routes = require 'app/routes/web.php';
+        foreach ($routes as $route => $params) {
+            $this->add($route, $params);
+        }
 
+        print_r(trim($_SERVER['REQUEST_URI'], '/'));
+
+    }
+
+    protected function add($route, $params): void
+    {
+        $route = '#^' . $route . '$#';
+        $this->routes[$route] = $params;
+    }
+
+    protected function matchRoute()
+    {
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+
+        foreach ($this->routes as $route => $params) {
+
+            if (preg_match($route, $url, $matches)) {
+
+                $this->params = $params;
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function run()
+    {
+        if ($this->matchRoute()) {
+            $controller = 'application\controllers\\' . ucfirst($this->params['controller']) . 'Controller';
+            if (class_exists($controller)) {
+                $action = $this->params['action'];
+                if (method_exists($controller, $action)) {
+                    $controller = new $controller($this->params);
+                    $controller->$action();
+                } else {
+                    echo '404';
+                }
+            } else {
+                echo '404';
+            }
+        } else {
+            echo '404';
+        }
     }
 
 }
