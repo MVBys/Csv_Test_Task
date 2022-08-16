697079ed17e920151dd6dfca16a11e1453002ab2
MVbys <bismaxmail@gmail.com>
2022-08-16 21:23:32 +0300

    Add component amin nav in view

---
+++ app/views/components/main_nav.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
+  <div class="container-fluid">
+
+    <div class="collapse navbar-collapse justify-content-md-center">
+      <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <a class="nav-link" href="/index">Import data</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="/records">View results</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
